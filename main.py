62798d779722839683d0c2bb3d8f53baa6dfa42d
Riku Sjöroos <riku.sjoroos@huld.io>
2024-08-06 20:41:14 +0300

    viimeisimmät muutokset

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ class Game:
         self.player_list = []
         self.turn_idx = 0
         self.started_idx = 0
+        self.starting_points = 0
 
-    
     def create_players(self, number_of_players, points, window):
         self.number_of_players = number_of_players
-        
+        self.starting_points = points
+        self.player_list = []
         for player in range(number_of_players):
             player = Player(points)
             self.player_list.append(player)
-        print(self.player_list)
         window['-TURN-'].update("Player 1 points")
         window['-POINTSLEFT1-'].update(background_color = 'white', text_color = 'black')
 
@@ -36,27 +36,98 @@ class Game:
             points_scored = int(window[f'-POINTS-'].get())
         except ValueError:
             return False
-
-        self.player_list[self.turn_idx].points -= points_scored
-        window[f'-POINTSLEFT{self.turn_idx+1}-'].update(str(self.player_list[self.turn_idx].points))
+        if points_scored > 180:
+            return False
+        player = self.player_list[self.turn_idx]
+        player.points -= points_scored
+        player.leg_scores.append(points_scored)
+        player.match_scores.append(points_scored)
+        window[f'-POINTSLEFT{self.turn_idx+1}-'].update(str(player.points))
         window['-POINTS-'].update("")
+        self.update_stats(window)
         return True
 
+    def check_winner(self, window):
+        player = self.player_list[self.turn_idx]
+        if player.points == 0:
+            playername = window[f'-PLAYER{self.turn_idx+1}NAME-'].get()
+            answer = sg.popup_yes_no(playername + " won?", title = "Winner?")
+            if answer == "Yes":
+                self.started_idx += 1
+                if self.started_idx > self.number_of_players - 1:
+                    self.started_idx = 0
+                player.legwins += 1
+                window[f'-PLAYER{self.turn_idx + 1}LEGWIN-'].update(str(player.legwins))
+                if player.hco < player.leg_scores[-1]:
+                    player.hco = player.leg_scores[-1]
+                self.turn_idx = self.started_idx -1
+                for i in range(self.number_of_players):
+                    window[f'-POINTSLEFT{i+1}-'].update(str(self.starting_points))
+                    self.player_list[i].points = self.starting_points
+                    self.player_list[i].leg_scores = []
 
+                self.update_stats(window)
 
-    def check_winner(self):
-        self.started_idx += 1
+            if answer == "No":
+                player.leg_scores.pop()
+                player.match_scores.pop()
+                player.leg_scores.append(0)
+                player.match_scores.append(0)
+    
+    def update_stats(self, window):
+        for i in range(self.number_of_players):
+            player = self.player_list[i]
+            if len(player.leg_scores) > 0:
+                player.firstnine = sum(player.leg_scores[:3]) / len(player.leg_scores[:3])
+                player.avg = sum(player.leg_scores) / len(player.leg_scores)
+                player.lscore = player.leg_scores[-1]
+            if len(player.match_scores) > 0:
+                player.match_avg = sum(player.match_scores) / len(player.match_scores)
 
+            window[f'-P{i + 1}AVG-'].update(str(round(player.avg, 2)))
+            window[f'-P{i + 1}MATCHAVG-'].update(str(round(player.match_avg, 2)))
+            window[f'-P{i + 1}FIRSTNINEAVG-'].update(str(round(player.firstnine, 2)))
+            window[f'-P{i + 1}CO-'].update(str(player.hco))
+            window[f'-P{i + 1}LSCORE-'].update(str(player.lscore))
+    
+    def go_back(self, window):
+        print(self.turn_idx)
+        if len(self.player_list[self.turn_idx-1].leg_scores) == 0:
+            return
+
+        if self.turn_idx == 0:
+            self.turn_idx = self.number_of_players - 1
+        else:
+            self.turn_idx -= 1
+
+        player = self.player_list[self.turn_idx]
+        if len(player.leg_scores) > 0:
+            player.lscore = player.leg_scores.pop()
+            player.points += player.lscore
+
+        if len(player.match_scores) > 0:
+            player.match_scores.pop()
+        for i in range(self.number_of_players):
+            window[f'-POINTSLEFT{i+1}-'].update(background_color = sg.theme_background_color(), text_color = sg.theme_text_color())
+
+        window[f'-P{self.turn_idx + 1}LSCORE-'].update(str(player.lscore))
+        window[f'-POINTSLEFT{self.turn_idx + 1}-'].update(str(player.points), background_color = 'white', text_color = 'black')
+        window['-TURN-'].update(f"Player {self.turn_idx+1} points")
+        window[f'-POINTS-'].update(str(player.lscore))
+        
 class Player:
     def __init__(self, points: int):
         self.points = points
+        self.match_scores = []
+        self.leg_scores = []
         self.turn = False
         self.avg = 0
         self.match_avg = 0
         self.firstnine = 0
+        self.legwins = 0
+        self.hco = 0
+        self.lscore = 0
 
-    def scored(self, scored_points: int):
-        self.points = self.points - scored_points
 
 def create_layout_for_players(window, num_players, startingpoints):
     # Luodaan layout pelaajien määrän perusteella
@@ -65,20 +136,22 @@ def create_layout_for_players(window, num_players, startingpoints):
     layout.append([sg.Menu(menu_def, tearoff = False)])
     players = []
     result = []
-    xpad = 50
+    xpad = 10
     ypad = 50
-    if num_players > 0 and num_players < 4:
+    if num_players > 0 and num_players < 3:
         xpad = 100
-        ypad = 50
-    elif num_players == 5:
-        xpad = 10
+    elif num_players > 3:
+        xpad = 2
 
     for i in range(num_players):
         player_layout = [[sg.Text(f"Player {i+1}")],
-        [sg.InputText(size=(10,1), font=playerFont, key = f'-PLAYER{i+1}NAME-'), sg.Text("0", font=playerFont, key = f'-PLAYER{i+1}LEGWIN-')],
+        [sg.InputText(size=(8,1), font=playerFont, key = f'-PLAYER{i+1}NAME-'), sg.Text("0", font=playerFont, key = f'-PLAYER{i+1}LEGWIN-')],
         [sg.Text(startingpoints, key = f'-POINTSLEFT{i+1}-', font = pointFont, enable_events=True)],
         [sg.Text("AVG:"),sg.Text("0", key = f'-P{i+1}AVG-')],
-        [sg.Text("Match AVG:"), sg.Text("0", key = f'-P{i+1}MATCHAVG-')]
+        [sg.Text("Match AVG:"), sg.Text("0", key = f'-P{i+1}MATCHAVG-')],
+        [sg.Text("F9 AVG:"), sg.Text("0", key = f'-P{i+1}FIRSTNINEAVG-')],
+        [sg.Text("Highest CO:"), sg.Text("0", key = f'-P{i+1}CO-')],
+        [sg.Text("Latest Score:"), sg.Text("0", key = f'-P{i+1}LSCORE-')]
         ]
             
             # Tässä voit lisätä muita pelaajaa koskevia komponentteja
@@ -86,7 +159,7 @@ def create_layout_for_players(window, num_players, startingpoints):
         players.append(sg.Column(player_layout,justification='c', pad=(xpad,ypad)))
     
     result_column = [[sg.Text("Player 1 points", key = '-TURN-')],
-    [sg.InputText(size=(15,1), key = '-POINTS-'),sg.Button('Subtract', visible= True, bind_return_key=True), sg.Button("Remains", visible = True)],
+    [sg.InputText(size=(15,1), key = '-POINTS-'),sg.Button('Subtract', visible= True, bind_return_key=True), sg.Button("Remains", visible = False)],
     [sg.Button('Back', visible = True)]
     ]
 
@@ -110,7 +183,7 @@ def new_game():
     window = sg.Window("New Game", new_game_window_layout, modal = True)
     while True:
         event, values = window.read()
-        if event == exit or event == sg.WIN_CLOSED:
+        if event == 'exit' or event == sg.WIN_CLOSED:
             break
         elif event == 'OK':
             numberofplayers = int(window['-NUMBEROFPLAYERS-'].get())
@@ -131,7 +204,7 @@ def main():
 
     game = Game()
     menu_def = [['File', ['New Game']]]
-    window = sg.Window('Darts Counter', layout=[[sg.Menu(menu_def, tearoff=False,key='-MENU BAR-')]], resizable=True)
+    window = sg.Window('Darts Counter', layout=[[sg.Menu(menu_def, tearoff=False,key='-MENU BAR-')]], resizable=True, finalize=True, size=(200,200))
     while True:
         event, values = window.read()
         if event == sg.WINDOW_CLOSED:
@@ -144,10 +217,12 @@ def main():
             game.create_players(numberofplayers, int(startingpoints), window)
             print(numberofplayers, startingpoints)
         elif event == 'Subtract':
-
             if game.scored(window) == False:
                 continue
+            game.check_winner(window)
             game.change_turn(window)
+        elif event == "Back":
+            game.go_back(window)
 
     window.close()
 
